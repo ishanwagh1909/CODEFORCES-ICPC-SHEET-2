452acdd485897b8b0cf148b35cb6b59dfe66a002
ishanwagh1909 <ishanwagh03@gmail.com>
2024-08-20 00:03:25 +0530

    fixed_password

---
+++ PROBLEM_D.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main()
+{
+    int n;
+    while(cin>>n)
+    {
+        if(n==1999)
+        {
+            cout<<"Correct";
+            return 0;
+        }
+        else
+        {
+            cout<<"Wrong"<<endl;
+        }
+    }
+}
